a877dba1cf58ec2a845e04d7922adff15106808d
Preetham00x <preetham2006@outlook.com>
2026-05-28 13:38:37 +0530

    creating a class which implements commandline runner to demonstrate the internal autowiring

--- src/main/java/com/telidu/DatajpaApplication.java
+++ src/main/java/com/telidu/DatajpaApplication.java
@@ -1,4 +1,4 @@
-package com.telidu.DATAJPA;
+package com.telidu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/telidu/model/Customer.java
+++ src/main/java/com/telidu/model/Customer.java
@@ -1,5 +1,46 @@
 package com.telidu.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Customer {
+	@Id
+	private Integer id;
+	private String name;
+	private String city;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	@Override
+	public String toString() {
+		return "Customer [id=" + id + ", name=" + name + ", city=" + city + "]";
+	}
+	public Customer(Integer id, String name, String city) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.city = city;
+	}
+	public Customer() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
 
 }
